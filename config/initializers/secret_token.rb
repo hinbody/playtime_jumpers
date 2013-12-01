c37f09e385efb24db817bcbba2d6b1309ee2e1c2
Harley Inbody <hinbody@gmail.com>
2013-12-01 18:08:47 -0500

    Initial commit. Add rails_12factor gem and remove username from config/database.yml for heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PlaytimeJumpers::Application.config.secret_key_base = '2a7f8cc4df1a312361983fa38a1eae9677250f72be9365627b0765f85e347629a6cd18dd67fd8f7bbd3051a8a049de09845c617109bf5de469ca9ecfc376bf6e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PlaytimeJumpers::Application.config.session_store :cookie_store, key: '_playtime_jumpers_session'
