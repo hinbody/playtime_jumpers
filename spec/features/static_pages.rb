c1be22a4e361ee3adf8ec8cb690559ffc38c05a0
Harley Inbody <hinbody@gmail.com>
2014-01-04 13:29:26 -0500

    install rspec and start first feature
    Install rspec binstub. Begin spec for viewing the home page and write code to
    get spec to pass

---
+++ spec/features/static_pages.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+feature 'Static Pages' do
+  scenario "can visit root page" do
+    visit '/'
+
+    expect(page).to have_content('Michiana\'s home for bounce house rentals')
+  end
+end
